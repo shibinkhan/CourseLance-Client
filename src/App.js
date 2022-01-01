9fed5a909b7b0c912d9ecf58845e931937b88313
ZèD <imzihad@gmail.com>
2022-01-02 00:15:17 +0600

    Router: Added home and auth route with component skeleton
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Authentication from './components/Authentication/Authentication';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="authentication" element={<Authentication />} />
+      </Routes>
+    </React.Fragment>
   );
 }
 
